b06535d6df86f1a72838bbc4c6d00ce2a0dc8b9a
JANGHYUNn <kimjh2369@gmail.com>
2020-09-28 01:35:31 +0900

    no message

---
+++ Expo/simpleTodo/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'no-empty-function': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'no-undef': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        jsxBracketSameLine: true,
+      },
+    ],
+  },
+};
