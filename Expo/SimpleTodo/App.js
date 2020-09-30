f3349be60d67ca595e110ffd1d67109dc2966133
JANGHYUNn <kimjh2369@gmail.com>
2020-10-01 03:14:46 +0900

    수정: 삭제 체크 추가

--- Expo/SimpleTodo/App.js
+++ Expo/SimpleTodo/App.js
@@ -1,9 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components';
+import Constants from 'expo-constants';
+import { Platform } from 'react-native';
+import styled from 'styled-components/native';
+import AsyncStorage from '@react-native-community/async-storage';
 
 const Container = styled.SafeAreaView`
+  flex: 1;
+  padding-top: ${Constants.statusBarHeight}px;
+`;
+
+const KeyboardAvoidingView = styled.KeyboardAvoidingView`
   flex: 1;
 `;
 
@@ -20,6 +26,7 @@ const InputContainer = styled.View`
 const TodoItem = styled.View`
   flex-direction: row;
   align-items: center;
+  margin-bottom: 10px;
 `;
 
 const TodoItemText = styled.Text`
@@ -35,27 +42,87 @@ const Input = styled.TextInput`
 `;
 const Button = styled.Button``;
 
+const Check = styled.TouchableOpacity`
+  margin-right: 4px;
+`;
+
+const CheckIcon = styled.Text`
+  font-size: 20px;
+`;
+
 export default function App() {
+  const [lists, setLists] = React.useState([]);
+  const [todo, setTodo] = React.useState('');
+
+  React.useEffect(() => {
+    AsyncStorage.getItem('todoLists').then(data => {
+      setLists(JSON.parse(data));
+    });
+  }, []);
+
+  const changeHandler = value => {
+    setTodo(value);
+  };
+
+  const pressHandler = () => {
+    if (!todo) return;
+    const newData = {
+      id: new Date().getTime().toString(),
+      todo,
+      done: false,
+    };
+    setLists([...lists, newData]);
+    setTodo('');
+    setAsyncStorage(newData);
+  };
+
+  const deleteTodo = id => {
+    const newLists = lists.filter(item => item.id !== id);
+    setLists(newLists);
+    setAsyncStorage(newLists, 'delete');
+  };
+
+  const setAsyncStorage = async (newData, flag) => {
+    let data;
+    if (flag) data = JSON.stringify(newData);
+    else data = JSON.stringify([...lists, newData]);
+
+    await AsyncStorage.setItem('todoLists', data);
+  };
+
+  const checkTodo = index => {
+    const data = [...lists];
+    data[index] = { ...data[index], done: !data[index].done };
+
+    setLists(data);
+    setAsyncStorage(data, 'modify');
+  };
+
   return (
     <Container>
-      <Contents>
-        <TodoItem>
-          <TodoItemText>할 일 목록 표시</TodoItemText>
-          <TodoItemButton title="삭제"></TodoItemButton>
-        </TodoItem>
-        <TodoItem>
-          <TodoItemText>할 일 목록 표시</TodoItemText>
-          <TodoItemButton title="삭제"></TodoItemButton>
-        </TodoItem>
-        <TodoItem>
-          <TodoItemText>할 일 목록 표시</TodoItemText>
-          <TodoItemButton title="삭제"></TodoItemButton>
-        </TodoItem>
-      </Contents>
-      <InputContainer>
-        <Input />
-        <Button title="전송" />
-      </InputContainer>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+        <Contents>
+          {lists.map((item, index) => (
+            <TodoItem key={item.id}>
+              <Check>
+                <CheckIcon>{item.done ? '✅' : '☑️'}</CheckIcon>
+              </Check>
+              <TodoItemText onPress={() => checkTodo(index)}>
+                {item.todo}
+              </TodoItemText>
+              <TodoItemButton
+                title="삭제"
+                onPress={() => deleteTodo(item.id)}
+              />
+            </TodoItem>
+          ))}
+        </Contents>
+        <InputContainer>
+          <Input onChangeText={value => changeHandler(value)} value={todo} />
+          <Button title="전송" onPress={pressHandler} />
+        </InputContainer>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
