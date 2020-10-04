2b714bcd0ca2223d26a3d5f81bfe5194457a2c1e
JANGHYUNn <kimjh2369@gmail.com>
2020-10-04 22:32:12 +0900

    검색 기능 추가

--- ReactNative/BoxOffice/navigatios/AppNavigator.js
+++ ReactNative/BoxOffice/navigatios/AppNavigator.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import BoxOfficeNavigator from './BoxOfficeNavigator';
+import SearchNavigator from './SearchNavigator';
 
 const Drawer = createDrawerNavigator();
 
 function AppNavigator() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="BoxOffice" component={BoxOfficeNavigator} />
+      <Drawer.Screen
+        name="BoxOffice"
+        component={BoxOfficeNavigator}
+        options={{drawerLabel: '박스 오피스'}}
+      />
+      <Drawer.Screen
+        name="Search"
+        component={SearchNavigator}
+        options={{drawerLabel: '영화 검색'}}
+      />
     </Drawer.Navigator>
   );
 }
--- ReactNative/BoxOffice/pages/BoxOffice.js
+++ ReactNative/BoxOffice/pages/BoxOffice.js
@@ -2,6 +2,9 @@ import React, {useEffect} from 'react';
 import {ActivityIndicator} from 'react-native';
 import styled from 'styled-components/native';
 import axios from 'axios';
+import Title from '../components/Title';
+import ListItem from '../components/ListItem';
+import MovieName from '../components/MovieName';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -11,29 +14,10 @@ const Contents = styled.ScrollView`
   flex: 1;
 `;
 
-const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-  margin: 8px 12px;
-`;
-
-const ListItem = styled.TouchableOpacity`
-  padding: 12px;
-  border-bottom-color: #e5e5e5;
-  border-bottom-width: 1px;
-  flex-direction: row;
-  align-items: center;
-`;
-
 const Rank = styled.Text`
   font-size: 14px;
   color: #999999;
   margin-right: 12px;
-`;
-
-const MovieName = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
 `;
 
 const key = '66899b7950addc4d0e08846d27609c3e';
